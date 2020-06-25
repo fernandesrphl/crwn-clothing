a0604bd3609063acb8fe6fba8e8a044e30a35e5a
Raphael Fernandes <raph@MacBook-Pro-de-Raphael-2.local>
2020-06-25 12:18:03 +0200

    add cart dropdow'

--- src/components/custom-button/costume-button.component.jsx
+++ src/components/custom-button/costume-button.component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './costume-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ?  'google-sign-in' : '' } custom-button`}
-     {...otherProps}>
-        {children}
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+    <button
+      className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+      {...otherProps}
+    >
+      {children}
     </button>
-)
+  );
+  
+  export default CustomButton;
 
-export default CustomButton;
