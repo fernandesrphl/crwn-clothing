ec183589d2dcf8fd8f4c520dd65d875bd4ea5c59
Raphael Fernandes <raph@MacBook-Pro-de-Raphael-2.local>
2020-06-25 10:50:28 +0200

    yes

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assests/crown.svg';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -17,8 +19,21 @@ const Header = () => (
             <Link className='option' to='/shop'>
                 CONTACT
             </Link>
+            {currentUser ? (
+              <div className='option' onClick={() => auth.signOut()}>
+                   SIGN OUT
+              </div>
+              ) : (
+             <Link className='option' to='/signin'>
+                  SIGN IN
+             </Link>
+           )}
         </div>
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
