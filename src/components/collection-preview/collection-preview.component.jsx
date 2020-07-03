0d2e49ffa2d1d5014247c654d92d51c6834fb343
Fernandes Raphael <fernandes.rphl@gmail.com>
2020-07-03 15:31:16 +0200

    reducer, cart dropdown, count etc

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
         <div className='preview'>
             {items
             .filter((item, idx) => idx < 4)
-            .map(({ id, ...orhterItemProps }) => (
-                <CollectionItem key={id} {...orhterItemProps} />
+            .map(( item) => (
+                <CollectionItem key={item.id} item={item} />
             ))}
         </div>
     </div>
